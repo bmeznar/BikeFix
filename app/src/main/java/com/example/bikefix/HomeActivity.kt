ea59cdf59a2f2185e37cbd6cfb78c730b5afe1d9
bmeznar <bmeznar@users.noreply.github.com>
2020-10-23 22:37:38 +0200

    Creating basic main menu

--- app/src/main/java/com/example/bikefix/HomeActivity.kt
+++ app/src/main/java/com/example/bikefix/HomeActivity.kt
@@ -2,10 +2,39 @@ package com.example.bikefix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.dropdown,menu);
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId;
+        if (id == R.id.home){
+            Toast.makeText(this, "Home clicked", Toast.LENGTH_SHORT).show()
+            return true
+        }
+        else if (id == R.id.forum){
+            Toast.makeText(this, "Forum clicked", Toast.LENGTH_SHORT).show()
+            return true
+        }
+        else{
+            Toast.makeText(this,"Bike Shops Nearby clicked",Toast.LENGTH_SHORT).show()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
